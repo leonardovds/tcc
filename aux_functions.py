7f41049cde724962be39bba7bb595769b7e5b7eb
leonardovds <40767168+leonardovds@users.noreply.github.com>
2020-02-09 22:14:21 -0300

    Add files via upload

--- aux_functions.py
+++ aux_functions.py
@@ -1,3 +1,7 @@
+import math
+
+tolerance = 1
+
 def write_file(*args):
     beacon_storage = open('beacons.txt', 'w')
     for beacon in args:
@@ -22,5 +26,69 @@ def average(beacon):
     for value in beacon:
         average_sum += value[1]
     return average_sum/len(beacon)
+
+def possible_positions(axis, step, pi, distance):
+    positions = []
+    i = 0
+    if axis == 'y':
+        while i < 25:
+            aux = math.sin(step) * distance
+            positions.append(aux)
+            step += pi/50
+            i += 1
+    else:
+        while i < 25:
+            aux = math.cos(step) * distance
+            positions.append(aux)
+            step += pi/50
+            i += 1
+    return positions            
+            
+def transform_positions(pos_list, pos_x, pos_y):
+    positions = []
+    for valores in pos_list:
+        new_pos_x = round(abs(pos_x - valores[0]),5) 
+        new_pos_y = round(abs(pos_y - valores[1]),5)
+        aux = [new_pos_x, new_pos_y]
+        positions.append(aux)
+    return positions
+        
+def avrg_pos(positions):
+    total = 0
+    for position in positions:
+        total += position[0] #valores da posicao x
+    avrg_x = total/len(positions)
+    total = 0
+    for position in positions:
+        total += position[1] #valores da posicao y
+    avrg_y = total/len(positions)
+    return avrg_x, avrg_y
+
+def find_positions(beacon_one, beacon_two):
+    global tolerance
+    i = 0
+    positions = []
+    while i < len(beacon_one):
+        if beacon_one[i][0] >= beacon_two[i][0] - tolerance and beacon_one[i][0] <= beacon_two[i][0] + tolerance and beacon_one[i][1] >= beacon_two[i][1] - tolerance and beacon_one[i][1] <= beacon_two[i][1] + tolerance:
+            aux = [beacon_one[i][0], beacon_one[i][1]]
+            positions.append(aux)
+        i += 1
+    return positions
+    
+def final_position(positions, beacon):
+    global tolerance
+    i = 0
+    pos_final = []
+    while i < len(positions):
+        j = 0
+        while j < len(beacon):
+            if positions[i][0] >= beacon[j][0] - tolerance and positions[i][0] <= beacon[j][0] + tolerance and positions[i][1] >= beacon[j][1] - tolerance and positions[i][1] <= beacon[j][1] + tolerance:
+                aux = [positions[i][0], positions[i][1]]
+                pos_final.append(aux)
+                break
+            j += 1
+        i += 1
+    return pos_final
+
         
     
--- db.py
+++ db.py
@@ -1,26 +1,23 @@
-'''beacon simple database''' 
+'''beacon simple database'''
 
-one = {"id":"fb:fe:6b:90:92:b4",
+b1 = {"id":"fb:fe:6b:90:92:b4",
         "pos_x": 0,
         "pos_y": 0
         }
 
-two = {"id":"eb:fd:db:e0:7a:ad",
+b2 = {"id":"eb:fd:db:e0:7a:ad",
         "pos_x": 0,
-        "pos_y": 0
+        "pos_y": 5
         }
 
-three = {"id":"ec:2d:ed:68:b7:79",
-         "pos_x": 0,
-         "pos_y": 0
+b3 = {"id":"ec:2d:ed:68:b7:79",
+         "pos_x": 5,
+         "pos_y": 5
          }
 
-four = {"id":"d9:15:36:69:13:d9",
-        "pos_x": 0,
+b4 = {"id":"d9:15:36:69:13:d9",
+        "pos_x": 5,
         "pos_y": 0
         }
 
-devices = [one,
-           two,
-           three,
-           four]
+
